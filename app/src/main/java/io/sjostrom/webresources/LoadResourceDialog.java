b4747d5114e4d52bfcad88b15ebf5bbcaa4d6020
Nick Sjostrom <nick.sjostrom12@gmail.com>
2016-11-26 10:58:32 -0500

    Progress bar to dialogfragment. Status messages. Pre-append with protocol if none. Icon filler.

---
+++ app/src/main/java/io/sjostrom/webresources/LoadResourceDialog.java
@@ -0,0 +1,47 @@
+package io.sjostrom.webresources;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class LoadResourceDialog extends DialogFragment {
+
+    private TextView mLoadingText;
+
+    public LoadResourceDialog() {
+        // Required empty public constructor
+    }
+
+    public static LoadResourceDialog newInstance() {
+        LoadResourceDialog fragment = new LoadResourceDialog();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.load_resource_dialog_fragment, container, false);
+
+        mLoadingText = (TextView) view.findViewById(R.id.loadingText);
+
+        return view;
+    }
+
+    public void setLoadingText(String text) {
+        if (mLoadingText != null) {
+            mLoadingText.setText(text);
+        }
+    }
+}
--- app/src/main/java/io/sjostrom/webresources/MainActivity.java
+++ app/src/main/java/io/sjostrom/webresources/MainActivity.java
@@ -1,6 +1,7 @@
 package io.sjostrom.webresources;
 
-import android.graphics.Picture;
+import android.graphics.Bitmap;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,10 +13,11 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
@@ -25,19 +27,19 @@ import org.json.JSONArray;
 
 import java.util.Date;
 
-import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = MainActivity.class.getSimpleName();
 
     FloatingSearchView mSearchView;
     Toolbar mToolbar;
-    ProgressBar mProgressBar;
     WebView webView;
+    TextView mResourceWeirdness;
 
     RecyclerView mResources;
     private ResourceListAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private LoadResourceDialog mLoadingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,24 +48,24 @@ public class MainActivity extends AppCompatActivity {
 
         mSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-
         mResources = (RecyclerView) findViewById(R.id.resource_results);
-        mResources.setHasFixedSize(true);
+        mResourceWeirdness = (TextView) findViewById(R.id.resource_weirdness);
+        webView = (WebView) findViewById(R.id.webview);
 
+        // Setup RecyclerView
+        mResources.setHasFixedSize(true);
         mAdapter = new ResourceListAdapter(this);
         mResources.setAdapter(mAdapter);
-
         mLayoutManager = new LinearLayoutManager(this);
         mResources.setLayoutManager(mLayoutManager);
 
-        webView = (WebView) findViewById(R.id.webview);
+        // Setup WebView
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebChromeClient(new MyWebChromeClient());
         webView.setWebViewClient(new MyWebViewClient());
 
-        // set search ime
+        // Setup SearchView
         ((TextView) mSearchView.findViewById(R.id.search_bar_text)).setImeOptions(EditorInfo.IME_ACTION_SEARCH);
         mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
             @Override
@@ -73,16 +75,47 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSearchAction(String query) {
-                mAdapter.clearList();
-                mProgressBar.setVisibility(View.VISIBLE);
-                mResources.setVisibility(View.GONE);
-                webView.loadUrl(query);
+                startTest(query);
                 Log.d(TAG, "onSearchAction(): " + query);
+            }
+
+            private void startTest(String query) {
+                startingNewTest();
+                if(query.indexOf("http") < 0) {
+                    query = "http://" + query;
+                }
+                webView.loadUrl(query + "?" + new Date().getTime());
             }
         });
 
         setupToolbar();
-        mProgressBar.setVisibility(GONE);
+
+        // Create Loading Dialog
+        mLoadingDialog = LoadResourceDialog.newInstance();
+    }
+
+    private void startingNewTest() {
+        // clear adapter
+        mAdapter.clearList();
+        // hide recyclerview
+        mResources.setVisibility(View.GONE);
+        // hide resource weirdness (error)
+        mResourceWeirdness.setVisibility((View.GONE));
+        // show loading dialog
+        showLoadingDialog();
+    }
+
+    private void showLoadingDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        mLoadingDialog.show(fm, "load_resource_dialog_fragment");
+    }
+
+    private void hideLoadingDialog() {
+        mLoadingDialog.dismiss();
+    }
+
+    private void setLoadingDialogText(String text) {
+        mLoadingDialog.setLoadingText(text);
     }
 
     public void setupToolbar() {
@@ -98,29 +131,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class MyWebViewClient extends WebViewClient {
+
         @Override
         public void onPageFinished(WebView view, String url) {
+            setLoadingDialogText("Success!\nGathering resources...");
             webView.loadUrl("javascript:alert(JSON.stringify(performance.getEntriesByType('resource')))");
         }
 
         @Override
-        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-            Log.d(TAG, errorCode + " " + description);
-            mProgressBar.setVisibility(View.GONE);
-            mResources.setVisibility(View.VISIBLE);
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            mSearchView.setSearchText(url.split("\\?")[0]);
+            setLoadingDialogText("Requesting\n'"+url.split("\\?")[0]+"'...");
+        }
+
+        @Override
+        public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+            Log.w(TAG, "Error: " + errorResponse.getReasonPhrase());
         }
     }
 
     private class MyWebChromeClient extends WebChromeClient {
-        private int retry = 0;
-
         @Override
         public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
             Log.d(TAG, message);
             try {
                 JSONArray resources = new JSONArray(message);
+                setLoadingDialogText("Success!\nGathering resources (" + resources.length() + ")...");
                 if(mAdapter.getItemCount() < resources.length()) {
                     mAdapter.setList(resources);
+                    mAdapter.notifyDataSetChanged();
 
                     new android.os.Handler().postDelayed(
                             new Runnable() {
@@ -129,14 +168,18 @@ public class MainActivity extends AppCompatActivity {
                                     webView.loadUrl("javascript:alert(JSON.stringify(performance.getEntriesByType('resource').sort(function(a,b){return a.initiatorType.localeCompare(b.initiatorType);})))");
                                 }
                             },
-                            3000);
+                            2000);
                 }
                 else {
                     mAdapter.setList(resources);
                     mAdapter.notifyDataSetChanged();
 
-                    mProgressBar.setVisibility(View.GONE);
-                    mResources.setVisibility(View.VISIBLE);
+                    hideLoadingDialog();
+
+                    if(mAdapter.getItemCount() > 0)
+                        mResources.setVisibility(View.VISIBLE);
+                    else
+                        mResourceWeirdness.setVisibility((View.VISIBLE));
 
                     Log.d(TAG, "resource count is the same. stopping tests.");
                 }
